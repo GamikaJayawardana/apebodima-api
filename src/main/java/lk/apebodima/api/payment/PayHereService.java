f4a0917d78f1084c997801f5914bcf516fd424e4
Gamika Jayawardana <gamikakj@gmail.com>
2025-10-29 23:49:43 +0530

    Remove PayHere payment service and implementation
    Deleted PayHereService and PayHereServiceImpl classes, removing PayHere payment integration from the codebase.

--- src/main/java/lk/apebodima/api/payment/PayHereService.java
+++
@@ -1,8 +0,0 @@
-package lk.apebodima.api.payment;
-
-import java.util.Map;
-
-public interface PayHereService {
-    Map<String, String> initiatePayment(PaymentRequestDto paymentRequestDto);
-    boolean processNotify(Map<String, String> notification);
-}
